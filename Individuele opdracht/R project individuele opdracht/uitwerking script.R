acc171cdc4e4213080778c018a4ce7e89eb8a406
rberger91 <robbinberger@gmail.com>
2016-11-10 22:35:45 +0100

    de shiny app gescheiden in een script. lees mij.
    Somehow worden de plots en input objecten niet getoond in de boxes.
    figure this out.

--- Individuele opdracht/R project individuele opdracht/uitwerking script.R
+++ Individuele opdracht/R project individuele opdracht/uitwerking script.R
@@ -22,87 +22,22 @@ tail(sort(table(ac_data$Type), decreasing = T), n = 10)
 
 #wat wil de beste man nou?  operators per type aircraft of beide los? 
 
-help("aggregate")
-agr_data <- (aggregate(ac_data$Type ~ ac_data$Operator , data = ac_data, FUN = length))
-head(sort(agr_data$`ac_data$Type`, decreasing = T))
+# help("aggregate")
+# agr_data <- (aggregate(ac_data$Type ~ ac_data$Operator , data = ac_data, FUN = length))
+# head(sort(agr_data$`ac_data$Type`, decreasing = T))
       #schiet niks op. bovenstaande is zelfde als de eerder uitgevoerde head 
 
+
+#verkennen van plot mogelijkheden
 barplot(head(sort(table(ac_data$Operator), decreasing = T), n = 10), ylim = c(0,200), col = "gray", las =2 )
 
 par(mar=c(10,4,1,1))
-barplot(head(sort(table(ac_data$Type),decreasing = T), n = 10), ylim = c(0,350), col = "lightgray", las = 3)
+barplot(head(sort(table(ac_data$Type),decreasing = T), n = 10), ylim = c(0,350), col = "lightgray", las = 2, 
+        main = "Aantal crashes per type vliegtuig", ylab = "aantal crashes", xlab = "type vliegtuig")
+
+
+help(par)
 
 
 
-library(shiny)
-library(shinydashboard)
-
-ui <- dashboardPage(
- 
-  header <- dashboardHeader(title = "my test dashboard", dropdownMenu(type =  "messages",
-         messageItem(from = "Sales", message = "Final Case study of Data visualisation course"),
-         messageItem(from = "Auteur", message = "created by Robbin Berger, 0817915")
-    )
-  ),
-  
-  
-  sidebar <- dashboardSidebar(
-    sidebarMenu(
-      menuItem("Welcome page", tabName = "welcome page",icon = icon("comment")
-               ),
-      
-      menuItem("Dashoard", tabName = "dashboard", icon = icon("dashboard"), badgeLabel = "new", badgeColor = "green")
-      )
-    
-    )
-  )
-  
-  
-  body <- dashboardBody(
-    tabItems(
-    tabItem(tabName = "Welcome page",
-            fluidRow(
-              box(title = "Welcome", solidHeader = T, status = "info", "blablablabla", br(), "blablabla", width = 12)
-              
-            )),
-    
-    tabItem(tabName = "dashboard",
-            fluidRow(
-              box(title = "Welcome", solidHeader = T, status = "info", "blablablabla", br(), "blablabla", width = 12)
-              ),
-            fluidRow(
-              box(title = "Plot 1", solidHeader = T, status = "success", plotOutput("output$Plot1" )  
-            )
-            )
-    )
-    # fluidRow(
-    #  box(plotOutput(outputId = "bpOperators")), 
-    #  box(Output(outputId = "bpTypes"))
-    # )
-    ),
- 
-  
-  
-  dashboardPage(header, sidebar, body)
-  
-)
-
-
-  
-server <- function(input, output) {
-  # output$bpOperators <- renderPlot({
-  #  par(mar=c(10,4,1,1))
-  #   barplot(head(sort(table(ac_data$Operator), decreasing = T), n = 10), ylim = c(0,200), col = "gray", las =2, main = 
-  #           "Top 10 crashed operators", xlab = "operators", ylab = "number of crashes", sub = "source: airplane crashes since 1908")
-  #     })
-  # 
-  # output$bpTypes <-renderPlot({
-  #   par(mar=c(10,4,1,1))
-  #   barplot(head(sort(table(ac_data$Type),decreasing = T), n = 10), ylim = c(0,350), col = "lightgray", las = 3, main = 
-  #             "top 10 crashed types of airplanes")
-  # })
-  output$Plot1 <- renderPlot(plot(1:40))
-}
-
-shinyApp(ui = ui, server = server)
 
